8486692e33847c448b84bb454ee41f722b3118cb
Patrik Macák <patrik.macak93@gmail.com>
2025-10-05 23:08:13 +0200

    feat: adding pck.json + uncomplete code evenOddPosNeg.js

---
+++ M0/1/evenOddPosNeg.js
@@ -0,0 +1,17 @@
+const checkNumber = 1;
+let posNeg = 0;
+let evenOdd = 0;
+
+if (checkNumber > 0){
+    let posNeg = "Number is positive"
+} else{
+    let posNeg = "Numebr is negative"
+};
+
+if (checkNumber % 2 === 0){
+    let evenOdd = "Number is even"
+} else {
+    let evenOdd = "Numebr is even"
+}
+
+console.log(posNeg + ' & ' + evenOdd);
